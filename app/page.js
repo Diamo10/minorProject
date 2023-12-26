483024aeed89196fe88065fab46f01c593fa72b5
Sugam Gelal <sugamgelal@Sugams-MacBook-Air.local>
2023-12-26 21:24:43 +0545

    home

--- app/page.js
+++ app/page.js
@@ -11,13 +11,13 @@ export default function Home() {
       <div className="m-4">
         <div className="flex justify-center mt-4">
           <div className="text-2xl font-bold text-olive ">
-            <span className="text-[32px]">Precision in every Invesment.</span>{" "}
+            <span className="text-[32px]">Precision in every Invesment.</span>
             <br />
             <span className="">Your sucsess is our algoritm's mission.</span>
           </div>
         </div>
         <div className=" flex justify-center text-xl">
-          Let our recommendation engine guide you to a <br />
+          Let our recommendation engine guide you to a <br/>
           prosperous financial future.
         </div>
       </div>
