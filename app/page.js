3612836bddbd53f225e8d88d6d4f6081c7bb0d72
Sugam Gelal <sugamgelal@Sugams-MacBook-Air.local>
2023-12-27 14:49:15 +0545

    style: about us, contact and footer

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
               stroke-linecap="round"
@@ -70,7 +70,7 @@ export default function Home() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
               stroke-linecap="round"
@@ -97,7 +97,7 @@ export default function Home() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
               stroke-linecap="round"
@@ -124,7 +124,7 @@ export default function Home() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
               stroke-linecap="round"
@@ -145,11 +145,9 @@ export default function Home() {
 
       <div className="border-b border-gray my-5 mx-4"></div>
 
-
-
       {/* About Section */}
       <section className="" id="about">
-        <div className="grid grid-cols-2 mx-6">
+        <div className="grid grid-cols-1 tablet:grid-cols-2 mx-6">
           <div className=" flex items-center">
             <Image
               src={aboutImage}
@@ -161,24 +159,57 @@ export default function Home() {
             />
           </div>
           <div className=" items-center">
-            <span className="font-bold flex text-xl text-center justify-center mt-[24px]">About us</span>
+            <span className="font-bold flex text-xl text-center justify-center mt-[24px]">
+              About us
+            </span>
             <div className="mt-8 ml-3">
-            <div className="text-justify">
-              {`Lorem Ipsum is simply dummy text of the printing and typesetting
+              <div className="text-justify">
+                {`Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
               electronic typesetting,`}
-            </div>
+              </div>
             </div>
           </div>
         </div>
       </section>
 
+      <div className="border-b border-gray my-5 mx-4"></div>
+
+      {/* contact section */}
+      <section id="contact" className="">
+        <p className="text-center font-bold text-xl mt-4">Contact us</p>
+        <div className="grid grid-cols-1 tablet:grid-cols-2 text-center mx-6">
+          <div className=""> First Name</div>
+          <div className=""> Second Name</div>
+
+        </div>
+        <div className="mx-6 text-center"> Email</div>
+        <div className="mx-6 text-center"> Message</div>
+        <div className="font-bold text-center mx-[80px] tablet:mx-[250px] bg-olive text-white p-[9px] rounded-lg  hover:font-extrabold cursor-pointer">
+            Send Message
+          </div>
+
+
+
+
+
+
+
+      </section>
+
 
       <div className="border-b border-gray my-5 mx-4"></div>
 
+      <section className="text-center" id="">
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+          <aside>
+            <p>Copyright © 2023 - All right reserved by Investor Recommendor System</p>
+          </aside>
+        </footer>
+      </section>
     </>
   );
 }
--- components/navbar.js
+++ components/navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
-              class="w-6 h-6 tablet:hidden"
+              className="w-6 h-6 tablet:hidden"
             >
               <path
                 stroke-linecap="round"
