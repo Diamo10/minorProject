2cb00d8b24deb9359a38af9d0f95543b887108bd
Sugam Gelal <sugamgelal@Sugams-MacBook-Air.local>
2023-12-27 12:19:36 +0545

    Style: landing page

--- app/page.js
+++ app/page.js
@@ -164,12 +164,12 @@ export default function Home() {
             <span className="font-bold flex text-xl text-center justify-center mt-[24px]">About us</span>
             <div className="mt-8 ml-3">
             <div className="text-justify">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
+              {`Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
-              electronic typesetting,
+              electronic typesetting,`}
             </div>
             </div>
           </div>
